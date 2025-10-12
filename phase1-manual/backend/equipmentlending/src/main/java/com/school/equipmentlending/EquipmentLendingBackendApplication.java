1ae11cbc6f0ffb278bb30071148c43bb16036e28
Akshada Tambe <akshada.tambe33@gmail.com>
2025-10-12 17:28:46 +0530

    [SELP-1]:created spring boot skeleton

---
+++ phase1-manual/backend/equipmentlending/src/main/java/com/school/equipmentlending/EquipmentLendingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.school.equipmentlending;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EquipmentLendingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EquipmentLendingBackendApplication.class, args);
+	}
+
+}
---
+++ phase1-manual/backend/equipmentlending/src/test/java/com/school/equipmentlending/EquipmentLendingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.school.equipmentlending;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EquipmentLendingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
